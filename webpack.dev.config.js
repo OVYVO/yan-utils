a572a084a64cc3bfacf6b7b405e828b5658f03a4
yangang <1357662425@qq.com>
2020-09-21 21:20:07 +0800

    fix: 添加babel处理ES6转ES5

---
+++ webpack.dev.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./src/utils.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  module:{
+    rules:[{
+      test:/\.js?$/,
+      exclude:/(node_modules)/,
+      loader:'babel-loader'
+    }]
+  },
+
+  plugins:[
+    new HtmlWebpackPlugin({
+      template:'./index.html'
+    })
+  ],
+  devServer:{
+    contentBase: path.join(__dirname,'./dist'), //根目录
+    open: true, //自动打开浏览器
+    port: 9000
+  }
+};
