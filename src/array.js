affc109a763f0d9d318d2f0573d65be4a9583068
OVYVO <1357662425@qq.com>
2020-09-22 21:25:06 +0800

    feat: 添加常见数组操作方法

--- src/array.js
+++ src/array.js
@@ -1,18 +1,64 @@
-/**
- * 数组取交集
- * @param arr_a：数组a
- * @param arr_b： 数组b
+/*
+ * @Author: yangang
+ * @Date: 2020-09-19 18:15:00
+ * @LastEditTime: 2020-09-22 21:20:54
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \yan-utils\src\array.js
  */
-const arrayIntersection = function (arr_a, arr_b)
-{
-  if (!Array.isArray(arr_a) || !Array.isArray(arr_b))
-  {
-    return console.error("请确保输入参数为数组类型");
-  }
+
+/**
+ * @description: 数组取交集
+ * @param {array}
+ * @return {array}
+ */
+const arrayIntersect = function (arr_a, arr_b){
   const IntersectValues = [...new Set(arr_a)].filter((item) =>
     arr_b.includes(item)
   );
   return IntersectValues;
 };
 
-export default { arrayIntersection };
+/**
+ * @description: 数组取差集
+ * @param {array}
+ * @return {array}
+ */
+const arrayDiffer = function(arr_a, arr_b){
+  const differValues = [...new Set([...arr_a, ...arr_b])].filter(item => !arr_b.includes(item) || !arr_a.includes(item)) 
+  return differValues;
+}
+
+/**
+ * @description: 数组去重
+ * @param {array} 
+ * @return {array} 
+ */
+const arrayUnique = function(arr){
+  const uniqueArr = [...new Set(arr)]
+  return uniqueArr
+}
+
+/**
+ * @description: 快速打平数组
+ * @param {array} 
+ * @return {array} 
+ */
+const arrayFlat = function(arr){
+  const flatArr =  arr.reduce(( pre, value ) => {
+    return Array.isArray(value) ? [...pre, ...flat(value)] : [...pre, value]
+  }, [])
+  return flatArr
+}
+
+/**
+ * @description: 随机获取数组项
+ * @param {array} 
+ * @return {array} 
+ */
+const arrayRandom = function(arr){
+  const  randomArr = arr[(Math.floor(Math.random() * (arr.length)))]
+  return randomArr
+}
+
+ export default { arrayIntersect,arrayDiffer,arrayUnique,arrayFlat,arrayRandom };
