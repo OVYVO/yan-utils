318ff73ff94a03d0c62af07929135fca9d257791
OVYVO <1357662425@qq.com>
2020-09-23 16:21:07 +0800

    feat: 添加对象方法

---
+++ src/object.js
@@ -0,0 +1,22 @@
+/**
+ * @description: 判断对象是否为空
+ * @param {object}
+ * @return {object}
+ */
+
+const isEmptyObject = function (obj)
+{
+  if (!obj || typeof obj !== 'object' || Array.isArray(obj))
+  {
+    return false
+  }
+  else
+  {
+    return !Object.keys(obj).length
+  }
+}
+
+export default
+{
+  isEmptyObject
+}
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import array from './array'
 import deepclone from './deepclone'
+import object from './object'
 
-export default { array, deepclone }
+export default { array, deepclone, object }
