0a961a8b1f65dc3589ed02a62a9be0b6828bfafc
OVYVO <1357662425@qq.com>
2020-10-18 09:50:39 +0800

    fix: fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 /*
  * @Author: your name
  * @Date: 2020-10-18 09:36:13
- * @LastEditTime: 2020-10-18 09:41:18
- * @LastEditors: your name
+ * @LastEditTime: 2020-10-18 09:50:27
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \yan-utils\src\index.js
+ * @Sync ID:bf50a5c4f82e7ee98816bcff88e4e385
  */
 import array from './array';
 import deepclone from './deepclone';
