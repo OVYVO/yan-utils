1de84150bc6b84147d5f79a79814def2418c65f1
OVYVO <1357662425@qq.com>
2020-10-18 11:13:44 +0800

    fix: 添加pagination.js用例

--- src/pagination.js
+++ src/pagination.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-10-18 10:51:39
- * @LastEditTime: 2020-10-18 11:05:21
+ * @LastEditTime: 2020-10-18 11:13:01
  * @LastEditors: Please set LastEditors
  * @Description: 分页函数
  * @FilePath: \yan-utils\src\pagenation.js
@@ -284,3 +284,39 @@ class Pagination {
 }
 
 export default Pagination;
+
+/*
+// 分页元素ID（必填）
+var selector = '#pagelist';
+
+// 分页配置
+var pageOption = {
+  // 每页显示数据条数（必填）
+  limit: 5,
+  // 数据总数（一般通过后端获取，必填）
+  count: 162,
+  // 当前页码（选填，默认为1）
+  curr: 1,
+  // 是否显示省略号（选填，默认显示）
+  ellipsis: true,
+  // 当前页前后两边可显示的页码个数（选填，默认为2）
+  pageShow: 2,
+  // 开启location.hash，并自定义hash值 （默认关闭）
+  // 如果开启，在触发分页时，会自动对url追加：#!hash值={curr} 利用这个，可以在页面载入时就定位到指定页
+  hash: false,
+  // 页面加载后默认执行一次，然后当分页被切换时再次触发
+  callback: function(obj) {
+    // obj.curr：获取当前页码
+    // obj.limit：获取每页显示数据条数
+    // obj.isFirst：是否首次加载页面，一般用于初始加载的判断
+
+    // 首次不执行
+    if (!obj.isFirst) {
+      // do something
+    }
+  }
+};
+
+// 初始化分页器
+new Pagination(selector, pageOption);
+ */
