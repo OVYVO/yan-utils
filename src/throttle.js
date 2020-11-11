a5630d4e794012490fa60c5813ac4258144e0608
OVYVO <1357662425@qq.com>
2020-11-11 15:24:38 +0800

    fix: 添加节流函数

--- src/throttle.js
+++ src/throttle.js
@@ -1,6 +1,11 @@
+/**
+ * @desc 节流函数 （等待时间以内触发多次只执行一次）
+ * @params 
+ * @return function
+ */
 function throttle(fn, wait) {
-  let time = 0,
-    timer = null
+  let time = 0
+  let timer = null
   return function () {
     let now = Date.now()
     let args = arguments
