661463ebd465602d2a5da0e64ac6327734093955
ryanYan <vue12306@163.com>
2024-05-13 10:16:30 +0800

    :recycle: refactor: 重构copy及download方法

---
+++ src/copy.ts
@@ -0,0 +1,37 @@
+/**
+ * 复制文本到剪贴板，兼容性写法。
+ * @param text 要复制的文本。
+ * @returns 若复制成功则返回`true`，否则返回`false`。
+ */
+async function copyToClipboardCompatible(text: string): Promise<boolean> {
+  if (navigator.clipboard && navigator.clipboard.writeText) {
+    // 尝试使用 Clipboard API
+    try {
+      await navigator.clipboard.writeText(text);
+      return true;
+    } catch (err) {
+      console.error("使用 Clipboard API 复制文本时出错:", err);
+    }
+  } else {
+    // 回退到使用 document.execCommand('copy')
+    const textArea = document.createElement("textarea");
+    textArea.value = text;
+    textArea.style.position = "absolute";
+    textArea.style.left = "-9999px"; // 隐藏textarea
+    document.body.appendChild(textArea);
+    textArea.select();
+    try {
+      const isSuccess = document.execCommand("copy");
+      document.body.removeChild(textArea);
+      return isSuccess;
+    } catch (err) {
+      console.error("使用 document.execCommand('copy') 复制文本时出错:", err);
+      document.body.removeChild(textArea);
+    }
+  }
+  return false; // 如果以上方法都失败
+}
+
+export default {
+  copyToClipboardCompatible,
+};
---
+++ src/downLoad.ts
@@ -0,0 +1,49 @@
+/**
+ * url形式下载文件
+ * @param url 地址。
+ * @param fileName 文件名。
+ */
+function downloadFileFromUrl(url: string, fileName: string): void {
+  if (
+    !url ||
+    typeof url !== "string" ||
+    !fileName ||
+    typeof fileName !== "string"
+  ) {
+    console.error("Invalid URL or file name.");
+    return;
+  }
+  fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
+      return response.blob();
+    })
+    .then((blob) => downFileFromBlob(blob, fileName))
+    .catch((error) => {
+      console.error("Error downloading file:", error);
+    });
+}
+
+/**
+ * 下载Blob数据为文件。
+ * @param blob Blob数据。
+ * @param fileName 文件名。
+ */
+function downFileFromBlob(blob: Blob, fileName: string): void {
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a") as HTMLAnchorElement;
+  a.style.display = "none";
+  document.body.appendChild(a);
+  a.href = url;
+  a.download = fileName;
+  a.click();
+  window.URL.revokeObjectURL(url); // 释放URL
+  document.body.removeChild(a); // 清理DOM
+}
+
+export default {
+  downloadFileFromUrl,
+  downFileFromBlob,
+};
